a650dd9fc482a251ebdc8ae9371bc9be5689ba4b
t <t>
2019-03-07 16:19:55 +0000

    tied everything together for db install and build

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #remove previous postgres versions
 brew uninstall --force postgresql
 
-#remove all previous postgres files
-rm -rf /usr/local/var/postgres
-
 #install postgres
 brew install postgres
 
-#start postgreSQL
-pg_ctl -D /usr/local/var/postgres start
+#remove all previous postgres files
+rm -rf /usr/local/var/postgres
 
 #initialise
 initdb /usr/local/var/postgres
+
+#start postgreSQL
+pg_ctl -D /usr/local/var/postgres start
 
 #create SCHISTO_DB
 createdb SCHISTO_PERSIST
